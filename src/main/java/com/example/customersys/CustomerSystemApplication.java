17621adeb75ce5c324e675664eb68732ef733b10
yanoshoki <yanopitrombone@gmail.com>
2025-06-15 21:51:15 +0900

    変更

--- src/main/java/com/example/customersys/CustomerSystemApplication.java
+++ src/main/java/com/example/customersys/CustomerSystemApplication.java
@@ -2,8 +2,10 @@ package com.example.customersys;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class CustomerSystemApplication {
 
 	public static void main(String[] args) {
